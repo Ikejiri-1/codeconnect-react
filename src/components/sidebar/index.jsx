e1bc183608bb239cf2ec108441124d21c9e36bb5
Ikejiri-1 <pedroike21@gmail.com>
2025-07-03 10:56:58 -0300

    sidebar-content

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,40 @@
+import Logo from "./assets/Logo.svg";
+import Feed from "./assets/feed.svg";
+import Info from "./assets/info.svg";
+import Account from "./assets/account_circle.svg";
+import Logout from "./assets/logout.svg";
+
+export default function SideBar() {
+  return (
+    <>
+      <aside>
+        <img src={Logo} alt="Logo da CodeConnect" />
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Publicar</a>
+            </li>
+            <li>
+              <a href="#">
+                <img src={Feed} alt="" />
+                <span>Feed</span>
+              </a>
+            </li>
+            <li>
+              <img src={Account} alt="" />
+              <span>Perfil</span>
+            </li>
+            <li>
+              <img src={Info} alt="" />
+              <span>Sobre nós</span>
+            </li>
+            <li>
+              <img src={Logout} alt="" />
+              <span>Sair</span>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
+  );
+}
