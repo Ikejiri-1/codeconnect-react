a3dbc5aa8c6207a793364001fe1043a0ec61a7f9
Ikejiri-1 <pedroike21@gmail.com>
2025-07-03 13:05:33 -0300

    sidebar-done

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -3,37 +3,40 @@ import Feed from "./assets/feed.svg";
 import Info from "./assets/info.svg";
 import Account from "./assets/account_circle.svg";
 import Logout from "./assets/logout.svg";
+import "./styles.css";
 
 export default function SideBar() {
   return (
     <>
       <aside>
         <img src={Logo} alt="Logo da CodeConnect" />
-        <nav>
-          <ul>
+        <nav className="menu">
+          <ul className="menu-list">
             <li>
-              <a href="#">Publicar</a>
+              <a href="#" className="item-button">
+                Publicar
+              </a>
             </li>
             <li>
-              <a href="#">
+              <a href="#" className="item-link">
                 <img src={Feed} alt="" />
                 <span>Feed</span>
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="#" className="item-link">
                 <img src={Account} alt="" />
                 <span>Perfil</span>
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="#" className="item-link">
                 <img src={Info} alt="" />
                 <span>Sobre nós</span>
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="#" className="item-link">
                 <img src={Logout} alt="" />
                 <span>Sair</span>
               </a>
