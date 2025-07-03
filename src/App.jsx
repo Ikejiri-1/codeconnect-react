a16479a67606c5e189e7b6912e03d7b725468275
Ikejiri-1 <pedroike21@gmail.com>
2025-07-03 15:17:49 -0300

    Ordenation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import SearchBar from "./components/SearchBar";
 import SideBar from "./components/sidebar";
 import "./App.css";
 import Filter from "./components/Filter";
+import Ordenation from "./components/Ordenation";
 function App() {
   return (
     <div className="container">
@@ -9,6 +10,7 @@ function App() {
       <div className="content">
         <SearchBar />;
         <Filter />
+        <Ordenation />
       </div>
     </div>
   );
---
+++ src/components/Ordenation/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Ordenation() {
+  return (
+    <section className="ordenation">
+      <ul className="ordenation-list">
+        <li className="ordenation-item ordenation-item--active">Recentes</li>
+        <li className="ordenation-item">Recentes</li>
+      </ul>
+    </section>
+  );
+}
