53d221e572d18e658d2885657fd7f0ae168c345c
Ikejiri-1 <pedroike21@gmail.com>
2025-07-03 17:35:13 -0300

    fix width and loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,45 @@ import Card from "./components/Card";
 import { useEffect, useState } from "react";
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch(
       "https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes"
     )
       .then((response) => response.json())
       .then((data) => setData(data));
+    setLoading(false);
   }, []);
   console.log(data);
   return (
     <div className="container">
-      <SideBar />;
+      <SideBar />
       <div className="content">
-        <SearchBar />;
+        <SearchBar />
         <Filter />
         <Ordenation />
-        <ul className="cards-list">
-          {data
-            ? data.map((item, index) => (
-                <li key={index}>
-                  <Card
-                    id={item.id}
-                    imageURL={item.imagem_capa}
-                    title={item.titulo}
-                    resume={item.resumo}
-                    codeLines={item.linhas_de_codigo}
-                    share={item.compartilhamentos}
-                    comments={item.comentarios}
-                    user={item.usuario}
-                  />
-                </li>
-              ))
-            : null}
-        </ul>
+        {loading ? (
+          <div className="loading-spinner"></div>
+        ) : (
+          <ul className="cards-list">
+            {data
+              ? data.map((item, index) => (
+                  <li key={index}>
+                    <Card
+                      id={item.id}
+                      imageURL={item.imagem_capa}
+                      title={item.titulo}
+                      resume={item.resumo}
+                      codeLines={item.linhas_de_codigo}
+                      share={item.compartilhamentos}
+                      comments={item.comentarios}
+                      user={item.usuario}
+                    />
+                  </li>
+                ))
+              : null}
+          </ul>
+        )}
       </div>
     </div>
   );
